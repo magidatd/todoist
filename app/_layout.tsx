dd99a4f82075d75b5397260bb02bff7c04754a4a
Daniel Magida <daniel.t.magida@gmail.com>
2025-04-27 18:47:13 +0200

    feat: add initial Color constants and Cache utility

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
 
-export default function RootLayout() {
-  return <Stack />;
-}
+const RootLayout = () => {
+	return <Stack />;
+};
+
+export default RootLayout;
